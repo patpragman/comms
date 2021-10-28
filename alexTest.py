b73b641e173591d8b4870d8792d5860422081fe0
Alex Rybicki <alex11587@gmail.com>
2021-10-28 11:29:55 -0700

    Testing new edit_password function

--- alexTest.py
+++ alexTest.py
@@ -2,12 +2,14 @@ import process
 import sql
 
 #Adding a user via hardcoded data. Change the username before uncommenting and running this
-#sampleDict = {"username" : "bigoldick", "password" : "Password"}
-#process.new_user(sampleDict)
+#sampleDict = {"username" : "hogrider69", "password" : "Password"}
+#print(process.new_user(sampleDict)) # Wrapped in a print statement to get the return value
 
 
 # This was me just testing my check_user function.
 # This function normally runs as part of process.new_user
 # to verify a user was added.
 
-print(sql.check_user("serioussam"))
+
+# now testing password updater
+process.edit_password('neofromthematrix')

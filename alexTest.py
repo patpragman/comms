04997a4ca9dc2e42ef3a9df24eeff95c5faefa27
Alex Rybicki <alex11587@gmail.com>
2021-10-28 19:12:59 -0700

    More testing

--- alexTest.py
+++ alexTest.py
@@ -6,6 +6,6 @@ import sql
 #print(process.new_user(sampleDict)) # Wrapped in a print statement to get the return value
 
 
-
+sampleDict2 = {"username" : "hogrider69", "password" : "Password469"}
 # now testing user deleter. Create a user with the code above and then delete them with the code below
-print(process.delete_user("hogrider69"))
+print(process.edit_password(sampleDict2))
